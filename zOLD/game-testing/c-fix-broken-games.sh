92b4e57306cfdaa7d7322600a65a0ee814ffc389
Tyler Wong <tylerwong.j@gmail.com>
2025-07-08 16:10:30 -0700

    main - latest

---
+++ zOLD/game-testing/c-fix-broken-games.sh
@@ -0,0 +1,5 @@
+ claude "please checkout cluade md and common errors to figure out hwo to fix the
+  games - use the games in /games folder to know what the best games are -
+  please update the games in /games-not-yet-tested and make sure they dont
+  have join screens and that they follow the same pratcies as the games in
+  /games folder"
