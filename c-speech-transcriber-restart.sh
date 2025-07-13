44ec46b4c1c71a4283c02caf67140df555e9c9a6
Tyler Wong <tylerwong.j@gmail.com>
2025-07-13 13:13:23 -0700

    main - speech transc

---
+++ c-speech-transcriber-restart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to restart speech transcriber service
+# Runs stop.sh first, then adds to login items
+
+SPEECH_DIR="$HOME/p2/speech-transcriber"
+
+echo "Stopping speech transcriber..."
+cd "$SPEECH_DIR" && ./stop.sh
+
+echo "Adding speech transcriber to login items..."
+cd "$SPEECH_DIR" && ./add-this-script-to-login-items.sh
+
+echo "Speech transcriber restart complete"
